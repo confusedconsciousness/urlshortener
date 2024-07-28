3f4671e4caad3864e33fab5f649a855f1fca1e30
Kishan Kumar <51268436+confusedconsciousness@users.noreply.github.com>
2024-07-28 14:53:25 +0530

    added support for datacenter id as well

--- src/main/java/com/kishan/core/KGS.java
+++ src/main/java/com/kishan/core/KGS.java
@@ -1,18 +1,21 @@
 package com.kishan.core;
+
+import java.util.stream.IntStream;
+
 /**
  * KGS: Key Generator Service This class provide us with unique key every time it is called We will
  * only be having one instance, so we can ideally
  */
 
 public class KGS {
-
   private int sequence = 0;
   private static final String BASE62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
   private final int machineId = 1;
+  private final int datacenterId = 1;
 
-  private String base62(long time, int machineId, int sequence) {
+  private String base62(long time, int machineId, int datacenterId, int sequence) {
     StringBuilder encodedString = new StringBuilder();
-    long modifiedNumber = Long.parseLong(String.format("%d%d%d", time, machineId, sequence));
+    long modifiedNumber = Long.parseLong(String.format("%d%d%d%d", time, machineId, datacenterId, sequence));
     while (modifiedNumber > 0) {
       int remainder = (int) (modifiedNumber % 62);
       encodedString.append(BASE62.charAt(remainder));
@@ -22,7 +25,16 @@ public class KGS {
   }
 
   public String getUniqueKey() {
-    sequence += 1; // this might overflow at some point in time
-    return base62(System.currentTimeMillis(), machineId, sequence);
+    // this is done to prevent overflowing
+    // in one millisecond we'll support at max 10,000 unique ids.
+    sequence = (sequence + 1) % 10000;
+    // inclusion of datacenterId, and machineId will definitely increase the unique key size by 1,
+    // but it can now support more than ever keys
+    return base62(System.currentTimeMillis(), machineId, datacenterId, sequence);
+  }
+
+  public static void main(String[] args) {
+    KGS kgs = new KGS();
+    IntStream.range(0,1000).forEach(i -> System.out.println(kgs.getUniqueKey()));
   }
 }
